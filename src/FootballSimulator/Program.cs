06f5815b5178a172ff8937105bde08134b2dc9a4
Erik Michiels <erik_michiels@hotmail.com>
2020-04-03 23:02:36 +0200

    Add Hello World application

---
+++ src/FootballSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FootballSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
